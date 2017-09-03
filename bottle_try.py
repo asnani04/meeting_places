e47c732b5e02bd954ce7fce253d2f7eb66ce56db
Anuj Nagpal <anujnag@iitk.ac.in>
2017-09-03 06:05:17 +0530

    list of results, three boxes

--- bottle_try.py
+++ bottle_try.py
@@ -28,7 +28,11 @@ def form_details():
     <br>
     Location 1: <input name="l1" type="text" /> <br> <br>
     Location 2: <input name="l2" type="text" /> <br> <br>
-    Type of meeting place: <input name="type_of_place" type="text" /> <br> <br>
+    Location 3: <input name="l3" type="text" /> <br> <br>
+    <div id="wrapper">
+    <button type="button" onclick="add_field()">Add more locations</button>
+    </div>
+    <br>Type of meeting place: <input name="type_of_place" type="text" /> <br> <br>
     Filter results on the basis of <select name="criteria">
     	<option value="travel_time">Travel Time</option>
     	<option value="waiting_time">Waiting Time</option>
@@ -36,20 +40,29 @@ def form_details():
   	</select> <br> <br>
     <input value="Find Places" type="submit" /> <br>
     </form>
+    <script>
+    function add_field() {
+    	var dummy = '<span>Label: <input type="text"><small>(ft)</small></span>\r\n';
+		document.getElementById('wrapper').innerHTML += dummy;
+    }
+    </script>
     '''
 
 @app.route('/index', method='POST')
 def find_places():
     initial_locations = []
-    for i in range(2):
+    for i in range(3):
         initial_locations.append(request.forms.get('l' + str(i+1)))
     type_of_place = request.forms.get('type_of_place')
 
     meetingObject = MeetingPlaces()
     meetingObject.connect_to_maps()
-    sorted_max_duration = meetingObject.find_meeting_places(
+    sorted_list = meetingObject.find_meeting_places(
         initial_locations, type_of_place)
-    return '<p>' + str(sorted_max_duration[0][0]) + '</p>'
+    string_req = ""
+    for j in range(len(sorted_list)):
+    	string_req = string_req + '<p>' + str(sorted_list[j][0]) + '</p><br>'
+    return string_req
     # return '''<p> This is my day. </p>'''
     
 run(app, host='localhost', port=8080, debug=True)
