d0b0f851d7bc29aef8315f4fbee3180af263a131
Anuj Nagpal <anujnag@iitk.ac.in>
2017-09-03 07:47:30 +0530

    Display Modifications

--- bottle_try.py
+++ bottle_try.py
@@ -10,20 +10,62 @@ def form_details():
     return '''
     <style>
     body {
-                background-color: #99ff99;
+        background-color: #F0E68C;
+        background-image: url("https://www.landmarkbangkok.com/images/dining/Huntsman-3.jpg");
     }
     #locations {
     	text-align: center;
     	padding: 2px;
     	border-radius: 4px;
     	background-color: lightgrey;
+    	margin-top: 200px;	
     	margin-left: 400px;
     	margin-right: 400px;
     	border-style: ridge;
     	border-width: 4px;
     	border-color: lightgrey;
 	}
+	ul {
+    	list-style-type: none;
+    	margin: 0;
+    	padding: 0;
+    	overflow: hidden;
+    	background-color: #333;
+    	position: fixed;
+    	top: 0;
+    	left: 0;
+    	width: 100%;
+	}
+
+	li {	
+    	float: left;
+	}
+
+	li a {
+    	display: block;
+    	color: white;
+    	text-align: center;
+    	padding: 14px 16px;
+    	text-decoration: none;
+	}
+
+	li a:hover:not(.active) {
+    	background-color: #111;
+	}
+
+	.active {
+	    background-color: #4CAF50;
+	}
     </style>
+
+    <ul>
+  		<li><a href="#home">Home</a></li>
+  		<li><a class="active" href="#letsmeet">Let's Meet</a></li>
+  		<li><a href="#contact">Contact</a></li>
+  		<li><a href="#about">About</a></li>
+  		<li style="float:right"><a class="active" href="#logout">Logout</a></li>
+	</ul>
+
     <form action="/index" id="locations" method="POST">
     <br>
     Location 1: <input name="l1" type="text" /> <br> <br>
@@ -38,8 +80,9 @@ def form_details():
     	<option value="waiting_time">Waiting Time</option>
     	<option value="rating">Rating</option>
   	</select> <br> <br>
-    <input value="Find Places" type="submit" /> <br>
+    <input value="Find Places" type="submit" /> <br><br>
     </form>
+    
     <script>
     function add_field() {
     	var dummy = '<span>Label: <input type="text"><small>(ft)</small></span>\r\n';
@@ -61,9 +104,70 @@ def find_places():
 
     sorted_list = meetingObject.find_meeting_places(
         initial_locations, type_of_place, pref)
-    string_req = ""
+    string_req = '''
+    <style>
+    body {
+        background-color: #F0E68C;
+        background-image: url("https://www.landmarkbangkok.com/images/dining/Huntsman-3.jpg");
+    }    
+    ul#navigation {
+    	list-style-type: none;
+    	margin: 0;
+    	padding: 0;
+    	overflow: hidden;
+    	background-color: #333;
+    	position: fixed;
+    	top: 0;
+    	left: 0;
+    	width: 100%;
+	}
+
+	.locations {
+    	text-align: center;
+    	padding: 5px;
+    	border-radius: 4px;
+    	background-color: lightgrey;
+    	margin-top: 1px;	
+    	margin-left: 2px;
+    	margin-right: 2px;
+    	margin-bottom: 0px;
+    	border-style: ridge;
+    	border-width: 4px;
+    	border-color: lightgrey;
+	}
+
+	li {	
+    	float: left;
+	}
+
+	li a {
+    	display: block;
+    	color: white;
+    	text-align: center;
+    	padding: 14px 16px;
+    	text-decoration: none;
+	}
+
+	li a:hover:not(.active) {
+    	background-color: #111;
+	}
+
+	.active {
+	    background-color: #4CAF50;
+	}
+    </style>
+
+    <ul id="navigation">
+  		<li><a href="#home">Home</a></li>
+  		<li><a href="#letsmeet">Let's Meet</a></li>
+  		<li><a class="active" href="#results">Results</a></li>
+  		<li><a href="#contact">Contact</a></li>
+  		<li><a href="#about">About</a></li>
+  		<li style="float:right"><a class="active" href="#logout">Logout</a></li>
+	</ul><br><br><br>
+    '''
     for j in range(len(sorted_list)):
-    	string_req = string_req + '<p>' + str(sorted_list[j][0]) + '</p><br>'
+    	string_req = string_req + '<div class="locations">' + str(sorted_list[j][0]) + '</div>'
     return string_req
     # return '''<p> This is my day. </p>'''
     
